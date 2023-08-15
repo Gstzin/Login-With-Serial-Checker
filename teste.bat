5d170dee50bb40c3f03fb6f612f3a71f0225ba52
Gst <119472470+Gstzin@users.noreply.github.com>
2023-08-15 19:05:18 -0300

    Update teste.bat

--- teste.bat
+++ teste.bat
@@ -1,9 +1,9 @@
 @echo off
 for /f "tokens=2 delims== " %%A in ('wmic baseboard get serialnumber /value ^| find /i "SerialNumber="') do set serialnumber=%%A
 
-if /i "%serialnumber%" == "You Serial Number" goto :exit
+if /i "%serialnumber%" == "Your Serial Number" goto :exit
 
-if /i not "%serialnumber%" == "You Serial Number" goto :exit2
+if /i not "%serialnumber%" == "Your Serial Number" goto :exit2
 
 
 :exit
